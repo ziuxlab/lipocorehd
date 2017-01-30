cb7ac8c2905702c4d48e36b41c5bcc31646651a7
Jorge mario <jmla196@gmail.com>
2017-01-30 10:44:30 -0500

    Update
    Problema de analytics solucionado

--- public/js/app.js
+++ public/js/app.js
@@ -1201,7 +1201,7 @@ wow.init();
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-89940020-1', 'auto');
+ga('create', 'UA-89940020-2', 'auto');
 ga('send', 'pageview');
 
 
